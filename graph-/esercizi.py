e4599a0f8d182539b25b541e26aa5fd5ee60629b
Dago86 <diego.deangelis@studio.unibo.it>
2019-04-15 18:59:15 +0200

    Delete esercizi.py

--- graph-/esercizi.py
+++
@@ -1,9 +0,0 @@
-dfs(graph G, Node u, boolean[] visitato)
-  S.push(u)
-    while(!S.isEmpty())
-        v = S.pop()
-            if(!visitato[u])
-                visitato[u] = true
-                for each v in G.adj(u)
-                    S.push(v)
-        
