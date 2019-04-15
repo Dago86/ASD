a253242feb3c78f08550aac729fa53077eddb36b
Dago86 <diego.deangelis@studio.unibo.it>
2019-04-15 18:59:41 +0200

    Update eserciziGrafi.py

--- graph-/eserciziGrafi.py
+++ graph-/eserciziGrafi.py
@@ -1,8 +1,8 @@
 dfs(graph G, Node u, boolean[] visitato)
   S.push(u)
-    while(!S.isEmpty())
+    while(not S.isEmpty())
         v = S.pop()
-            if(!visitato[u])
+            if(not visitato[u])
                 visitato[u] = true
                 for each v in G.adj(u)
                     S.push(v)
